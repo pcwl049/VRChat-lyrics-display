47d3f489e8492b8db1992fb310ad2c42739fc385
pcwl049 <pcwl049@gmail.com>
2026-03-06 22:15:23 +0800

    chore: 清理不必要的文件
    - 删除编译产物和可执行文件
    - 删除编译日志和临时文件
    - 删除 KuGouMusicApi 子模块
    - 只保留源代码和配置文件

--- cpp/run_build.bat
+++
@@ -1,7 +0,0 @@
-﻿@echo off
-chcp 65001 >nul
-call "D:\Program Files\VScode\VC\Auxiliary\Build\vcvars64.bat"
-if not exist "D:\Project\音乐显示\cpp\bin" mkdir "D:\Project\音乐显示\cpp\bin"
-echo Compiling GUI...
-cl /std:c++20 /EHsc /W3 /O2 /MD /I"D:\Project\音乐显示\cpp\third_party" "D:\Project\音乐显示\cpp\gui\main_gui.cpp" "D:\Project\音乐显示\cpp\gui\moekoe_ws.cpp" winhttp.lib runtimeobject.lib windowsapp.lib ws2_32.lib ole32.lib user32.lib gdi32.lib shell32.lib comctl32.lib dwmapi.lib psapi.lib /Fe:"D:\Project\音乐显示\cpp\bin\moekoe_gui.exe"
-echo Done. Exit code: %errorlevel%
--- cpp/temp_build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-call "D:\Program Files\VScode\VC\Auxiliary\Build\vcvars64.bat" >nul
-cd /d D:\Project\ÒôÀÖÏÔÊ¾\cpp
-if not exist bin mkdir bin
-echo Compiling...
-cl /std:c++20 /EHsc /W3 /O2 /MD /I"third_party" gui\main_gui.cpp gui\moekoe_ws.cpp winhttp.lib runtimeobject.lib windowsapp.lib ws2_32.lib ole32.lib user32.lib gdi32.lib shell32.lib comctl32.lib dwmapi.lib psapi.lib /Fe:bin\moekoe_gui.exe
-echo Exit code: %errorlevel%
-del /q *.obj 2>nul
